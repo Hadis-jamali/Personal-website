fba315e9fd053373ddc2b740bc2a06e532769aa1
Hadis Jamali <hadisjamali95@gmail.com>
2023-07-27 13:05:16 +0200

    add services

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./Components/header/Header";
 import About from "./Components/about/About";
+import Service from "./Components/services/Service";
+
+
 function App() {
   return (
     <>
       <Header></Header>
       <About></About>
+      <Service></Service>
     </>
   );
 }
